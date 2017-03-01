546838e60f9db9e016d5dcc5919c245797abb8d2
manuelbr <manuelbr@correo.ugr.es>
2017-03-01 18:06:53 +0100

    Finalización de la tarea 2. close #5

--- tarea_2/script.py
+++ tarea_2/script.py
@@ -1,14 +1,17 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from flask import Flask, render_template, make_response, send_file, request, session
+from flask import Flask, render_template, make_response, request, session, redirect, url_for
 import sys
 
 #Especifico el nombre y la localización de la carpeta de recursos estáticos
-app = Flask(__name__,static_url_path='/home/manuelbr/Dropbox/Sistemas Software Basados en Web/tarea_1/Static')
+app = Flask(__name__,static_url_path='')
 
 #Activamos el modo de depuración para el tiempo de desarrollo
 app.config['DEBUG'] = True
+
+#Especifico la clave secreta para poder mantener sesiones de usuarios abiertas
+app.secret_key = 'super secret key'
 
 #Especifico utf8 como el sistema de codificación por defecto
 #(Para evitar problemas con acentos y demás...)
@@ -18,13 +21,13 @@ sys.setdefaultencoding('utf8')
 #Página índice por defecto
 @app.route('/',methods=['GET', 'POST'])
 def login():
-    if 'email' in session:
+    if 'user' in session:
         #Si ya hay una sesión iniciada, se redirige a la página principal para que se le de la bienvenida
-        return principal(email=session['email'])
+        return principal(user=session['user'])
     elif request.method == 'POST':
         #Si se reciben los datos de email desde un login anterior, se envian a la página principal y además se registra en la sesión
-        session['email'] = request.form['email']
-        return principal(email=request.form['email'])
+        session['user'] = request.form['user']
+        return principal(user=request.form['user'])
     else:
         respuesta = make_response(render_template('login.html'))
         respuesta.headers['Content-Type'] = 'text/html; charset=utf-8'
@@ -32,53 +35,30 @@ def login():
 
 #Página principal que da la bienvenida al usuario
 @app.route('/principal')
-def principal(email):
-    respuesta = make_response(render_template('principal.html',email=email))
+def principal(user):
+    respuesta = make_response(render_template('principal.html',user=user))
     respuesta.headers['Content-Type'] = 'text/html; charset=utf-8'
     return respuesta
 
-#Devuelve un texto plano
-@app.route('/un_texto_plano')
-def holaMundo():
-    respuesta = make_response('Esto es un texto plano cualquiera')
-    respuesta.headers['Content-Type'] = 'text/plain; charset=utf-8'
-    return respuesta
-
-#Devuelve el template html que hay en la carpeta 'templates'
-@app.route('/contenido_html')
-def holaMundoHtml():
-    respuesta = make_response(render_template('hello.html'))
+#Página que muestra un texto y hereda de la principal
+@app.route('/muestraTexto')
+def muestraTexto():
+    respuesta = make_response(render_template('muestraTexto.html',user=session['user'],titulo='Esto es un título',contenido='Esto es una línea de texto plano'))
     respuesta.headers['Content-Type'] = 'text/html; charset=utf-8'
     return respuesta
 
-    #Versión del profesor
-    #usuarios = []
-    #usuarios.append({'name':'Pepe','dni':'123456'})
-    #usuarios.append({'name':'Paco','dni':'123456'})
-    #usuarios.append({'name':'','dni':'123456'})
-    #return render_template('hello.html',var='esto',usuarios=usuarios)
-
-#Devuelve una imagen para visualizarla en el navegador
-@app.route('/una_imagen')
-def holaMundoImagen():
-    filename = 'static/images/holaMundo.jpg'
-    respuesta = make_response(send_file(filename))
-    respuesta.headers['Content-Type'] = 'image/jpg'
+#Página que muestra una imagen y hereda de la principal
+@app.route('/muestraImagen')
+def muestraImagen():
+    respuesta = make_response(render_template('muestraImagen.html',user=session['user'],titulo='Título de la Imagen'))
+    respuesta.headers['Content-Type'] = 'text/html; charset=utf-8'
     return respuesta
 
-    #Forma alternativa del profesor
-    #f = open(filename)
-    #imagen = f.read()
-    #f.close()
-    #respuesta = make_response(imagen)
-    #return respuesta
-
-#Devuelve en texto plano aquello que escribimos a partir de 'este_texto_plano'
-@app.route('/este_texto_plano/<parametro>')
-def returnParametro(parametro):
-    respuesta = make_response('El parámetro que se ha pasado es: %s' % parametro)
-    respuesta.headers['Content-Type'] = 'text/plain; charset=utf-8'
-    return respuesta
+#Se deslogea al usuario de la sesión en la que se encuentra
+@app.route('/logout')
+def logout():
+    session.pop('user', None)
+    return redirect(url_for('login'))
 
 #Página de error
 @app.errorhandler(404)
