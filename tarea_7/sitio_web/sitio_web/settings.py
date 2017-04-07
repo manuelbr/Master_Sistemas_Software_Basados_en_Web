ae2be3f724b8e94fc46d69116c9dec858f4cf5bd
manuelbr <manuelbr@correo.ugr.es>
2017-04-07 12:14:48 +0200

    Inclusión del proceso de logs y arreglado de los fallos de logeo. Entrega final. close #26

--- tarea_7/sitio_web/sitio_web/settings.py
+++ tarea_7/sitio_web/sitio_web/settings.py
@@ -128,3 +128,52 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+
+LOG_FILE = 'logs.log'
+
+LOGGING = {
+      'version': 1,
+      'disable_existing_loggers': False,
+
+      'formatters': {
+
+          'verbose': {
+              'format' : "[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s",
+              'datefmt' : "%d/%b/%Y %H:%M:%S"
+          },
+
+          'simple': {
+              'format': '%(levelname)s [%(name)s:%(lineno)s] %(message)s'
+          },
+     },
+
+    'handlers': {
+
+        'file': {
+            'level': 'INFO',
+            'class': 'logging.FileHandler',
+            'filename': os.path.join(BASE_DIR, LOG_FILE),
+            'formatter': 'verbose',
+            'mode':'w'
+         },
+
+        'console': {
+             'level': 'DEBUG',
+             'class': 'logging.StreamHandler',
+            'formatter': 'simple'
+         }
+    },
+
+    'loggers': {
+        'django': {
+            'handlers':['file'],
+            'propagate': True,
+            'level':'ERROR',
+        },
+
+        'restaurantes': {
+            'handlers': ['file', 'console'],
+            'level': 'DEBUG',
+        },
+    }
+}
